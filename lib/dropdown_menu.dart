d73967b0bad80ee4c56a282304520bd4dcb2af8b
Paul <jpainam@gmail.com>
2019-12-19 15:58:10 +0800

    flutter dropdown menu

--- lib/dropdown_menu.dart
+++ lib/dropdown_menu.dart
@@ -1,7 +1,8 @@
 library dropdown_menu;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export '_src/dropdown_menu.dart';
+export '_src/dropdown_list_menu.dart';
+export '_src/drapdown_common.dart';
+export '_src/dropdown_header.dart';
+export '_src/dropdown_sliver.dart';
+export '_src/dropdown_templates.dart';
